682cf5402a144001d83696d4d66214a5cf93e114
khuddussk <skkhuddussk40@gmail.com>
2021-09-29 13:07:17 +0530

    PS Sprint 2
    Project Structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { Home } from "./components/Home";
-import { Nav } from "./components/Nav";
-import { Footer } from "./components/Footer";
+
 
 const App = () => {
   return (
     <div>
-      <Nav />
       <Home />
-      <Footer />
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,9 @@
-import { Personalinfo } from "./Home/Personalinfo";
-import { Professionalinfo } from "./Home/Professionalinfo";
-import { ImageGallary } from "./Home/ImageGallary";
 import "./Home.css";
 
 export const Home = () => {
   return (
     <div>
-      <table>
-        <tr>
-          <td>
-            <Personalinfo />
-          </td>
-          <td>
-            <Professionalinfo />
-          </td>
-        </tr>
-        <tr>
-          <td colSpan="2">
-            <ImageGallary />
-          </td>
-        </tr>
-      </table>
+    <h1>Welcome To Medi4sure App</h1> 
     </div>
   );
 };
